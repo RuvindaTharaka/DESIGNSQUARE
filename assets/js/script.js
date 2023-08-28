02191abef5aff747121c82bf35f2cb0fb5d90cae
RuvindaTharaka <ruvindapeiris23@gmail.com>
2023-08-28 15:04:03 +0530

    updated the changes3

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,37 +13,6 @@ function toggleNavLinks() {
 
 
 /* This is for active-links */
-const navLinks = document.querySelectorAll('.nav-links a');
-
-// Function to check if an element is in the viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Function to update the active link
-function updateActiveLink() {
-    navLinks.forEach(link => {
-        const sectionId = link.getAttribute('href');
-        const section = document.querySelector(sectionId);
-
-        if (section && isInViewport(section)) {
-            link.classList.add('active-link');
-        } else {
-            link.classList.remove('active-link');
-        }
-    });
-}
-
-// Attach scroll event listener to update active link
-window.addEventListener('scroll', updateActiveLink);
-
-
 
 /*Animation*/
 const animateOnScrollElements = document.querySelectorAll('.animate-on-scroll');
